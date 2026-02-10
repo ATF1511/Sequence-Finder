4318419f5a2e65f9967588a7b78974e216fb0071
ATF1511 <andrew.filmer@icloud.com>
2026-02-10 21:40:03 +0000

    Added the .py file that is only the function.
    Neat.

---
+++ Sequence Finder (Purely the function).py
@@ -0,0 +1,26 @@
+def find(a, b):
+    dhjszafkmiojdiow = a
+    sadknjfksnafksda = b
+    asiudhakdjakdhks = sadknjfksnafksda - dhjszafkmiojdiow
+
+    oerqoirjeioqfeio = dhjszafkmiojdiow - asiudhakdjakdhks
+
+    browhydoIhavenofriends = 'idk anymore'
+
+    if oerqoirjeioqfeio > 0:
+        browhydoIhavenofriends = '+'
+    elif oerqoirjeioqfeio < 0:
+        browhydoIhavenofriends = '-'
+
+        oerqoirjeioqfeio = 0 - oerqoirjeioqfeio
+    else:
+        browhydoIhavenofriends = 'because I code'
+
+    if oerqoirjeioqfeio == 'because I code':
+        print(str(asiudhakdjakdhks)+'n')
+        return(str(asiudhakdjakdhks)+'n')
+    
+    else:
+        print(str(asiudhakdjakdhks)+'n',str(browhydoIhavenofriends),str(oerqoirjeioqfeio))
+        return(str(asiudhakdjakdhks)+'n',str(browhydoIhavenofriends),str(oerqoirjeioqfeio))
+
