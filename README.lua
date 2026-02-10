b81222fdb2db7e4ea418f7973130dbc0178189f2
ATF1511 <andrew.filmer@icloud.com>
2026-02-10 21:37:10 +0000

    Added README.lua
    README.md is so basic it could neutralise hydrofluoric acid.

---
+++ README.lua
@@ -0,0 +1,19 @@
+--[[
+
+Hi
+So uhhh
+This script does what the title says. It can't handle squares and square roots and always prints it out in the form:
+Xn +/- Y 
+Like it works I guess
+Also it has very weird variable names but I didn't want to mess up any code that it might be added into.
+It doesn't need any imports to work so it SHOULD work in any python script.
+
+Also,
+You may be wondering why this is README.lua and not README.md .
+Eh I just think it's funny.
+I will probably make it for more languages... shouldn't be to complicated to transfer it to actual lua... right?
+
+Bye lol
+ATF1511
+
+]]--
