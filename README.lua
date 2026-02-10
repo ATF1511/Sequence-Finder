ddaf34f72d32a91455775b9c212092ffdaf1724c
ATF1511 <andrew.filmer@icloud.com>
2026-02-10 21:43:40 +0000

    Updated README.lua I guess
    Read the title. Just added a few extra notes.

--- README.lua
+++ README.lua
@@ -8,6 +8,9 @@ Like it works I guess
 Also it has very weird variable names but I didn't want to mess up any code that it might be added into.
 It doesn't need any imports to work so it SHOULD work in any python script.
 
+You will need to input the first two numbers of the sequence for it to work but I might make one that lets you do any two numbers. 
+Idk I've just had a horrible day and don't feel like it right now.
+
 Also,
 You may be wondering why this is README.lua and not README.md .
 Eh I just think it's funny.
